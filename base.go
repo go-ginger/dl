9f206cb4624dd50c79fca6deb0684e41cf63e91c
saeed <saeid.kamyabi71@gmail.com>
2019-08-15 15:19:20 +0430

    0

--- base.go
+++ base.go
@@ -3,15 +3,30 @@ package dl
 import "github.com/kulichak/models"
 
 type IBaseDbHandler interface {
-	Paginate(request *models.IRequest) (*models.PaginateResult, error)
+	BeforeInsert(request *models.Request)
+	Insert(request *models.Request) (*models.IBaseModel, error)
+	AfterInsert(request *models.Request)
+
+	BeforeQuery(request *models.Request)
+	Paginate(request *models.Request) (*models.PaginateResult, error)
+	Get(request *models.Request) (*models.IBaseModel, error)
+	AfterQuery(request *models.Request)
+
+	BeforeUpdate(request *models.Request)
+	Update(request *models.Request) error
+	AfterUpdate(request *models.Request)
+
+	BeforeDelete(request *models.Request)
+	Delete(request *models.Request) error
+	AfterDelete(request *models.Request)
 }
 
 type BaseDbHandler struct {
+	IBaseDbHandler
 }
 
-func (base *BaseDbHandler) Paginate(request *models.IRequest) (*models.PaginateResult, error) {
-	(*request).AddNewFilter("deleted", map[string]bool{
+func (base *BaseDbHandler) handleFilters(request *models.Request) {
+	request.AddNewFilter("deleted", map[string]bool{
 		"$ne": true,
 	})
-	return nil, nil
 }
