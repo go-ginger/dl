c61e3b65a3e46edf85c754f1ce870cd028a44384
saeed <saeid.kamyabi71@gmail.com>
2019-08-19 12:39:00 +0430

    fix event

--- event.go
+++ event.go
@@ -2,12 +2,12 @@ package dl
 
 type Event struct {
 	Name     string
-	Callback func(fieldName string, value interface{}) interface{}
+	Callback func(fieldName string, value interface{}) (interface{}, error)
 }
 
 var events = map[string]*Event{}
 
-func AddNewEvent(eventName string, callback func(fieldName string, value interface{}) interface{}) {
+func AddNewEvent(eventName string, callback func(fieldName string, value interface{}) (interface{}, error)) {
 	events[eventName] = &Event{
 		Name:     eventName,
 		Callback: callback,
@@ -17,7 +17,11 @@ func AddNewEvent(eventName string, callback func(fieldName string, value interfa
 func TryEvent(eventName, fieldName string, value interface{}) (result interface{}, ok bool) {
 	if event, exists := events[eventName]; exists {
 		ok = true
-		result = event.Callback(fieldName, value)
+		eventResult, err := event.Callback(fieldName, value)
+		if err != nil {
+			return nil, false
+		}
+		result = eventResult
 	}
 	return
 }
