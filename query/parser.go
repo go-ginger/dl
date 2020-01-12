23f3cdb1d12b0ff190d36a1f4c0d3f8d28f623d7
saeed <saeid.kamyabi71@gmail.com>
2020-01-12 13:49:04 +0330

    change parser methods structure

--- query/parser.go
+++ query/parser.go
@@ -5,9 +5,9 @@ import gm "github.com/go-ginger/models"
 type IParseResult interface {
 	GetQuery() (query interface{})
 	GetSort() (sort interface{})
-	GetParams() (params []interface{})
+	GetParams() (params interface{})
 }
 
 type IParser interface {
-	Parse(request gm.IRequest) (result IParseResult)
+	Parse(request gm.IRequest, extra ...interface{}) (result IParseResult)
 }
