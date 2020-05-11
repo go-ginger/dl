1cbdcb437979bea778437b2beae5f2049c470f85
Saeed <saeid.kamyabi71@gmail.com>
2020-05-11 21:28:36 +0430

    run afterQuery tryEvent threaded

--- read.go
+++ read.go
@@ -5,10 +5,33 @@ import (
 	"github.com/go-ginger/models"
 	"reflect"
 	"strings"
+	"sync"
 )
 
 func (base *BaseDbHandler) BeforeQuery(request models.IRequest) (err error) {
 	return
+}
+
+const (
+	CallerGet      = "GET"
+	CallerPaginate = "PAGINATE"
+)
+
+func (base *BaseDbHandler) performEvent(request models.IRequest, eventName, fieldName string, value interface{},
+	field *reflect.Value, wg *sync.WaitGroup) {
+	defer wg.Done()
+	result, handled := TryEvent(request, eventName, fieldName, value)
+	if handled && result != nil {
+		if field.CanSet() {
+			field.Set(reflect.ValueOf(result))
+		}
+	}
+}
+
+func (base *BaseDbHandler) beginHandleModelAfterQuery(request models.IRequest, model interface{},
+	isValue bool, remainingDepth int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	base.handleModelAfterQuery(request, model, isValue, remainingDepth)
 }
 
 func (base *BaseDbHandler) handleModelAfterQuery(request models.IRequest, model interface{},
@@ -26,38 +49,54 @@ func (base *BaseDbHandler) handleModelAfterQuery(request models.IRequest, model 
 
 	req := request.GetBaseRequest()
 	if doLoad, ok := req.Tags["load"]; !ok || doLoad {
+		callerType := request.GetTemp("caller_type")
 		switch s.Kind() {
 		case reflect.Struct:
+			wg := sync.WaitGroup{}
 			for i := 0; i < s.NumField(); i++ {
 				f := s.Field(i)
 				ff := sType.Field(i)
 				tag, ok := ff.Tag.Lookup("load_from")
+				if !ok {
+					if callerType == CallerGet {
+						tag, ok = ff.Tag.Lookup("get_load_from")
+					}
+					if !ok {
+						if callerType == CallerPaginate {
+							tag, ok = ff.Tag.Lookup("paginate_load_from")
+						}
+					}
+				}
 				if ok {
 					tagParts := strings.Split(tag, ",")
 					sourceFieldName, eventName := tagParts[0], tagParts[1]
 					sourceField := s.FieldByName(sourceFieldName)
 					val := sourceField.Interface()
-					result, handled := TryEvent(request, eventName, ff.Name, val)
-					if handled && result != nil {
-						if f.CanSet() {
-							f.Set(reflect.ValueOf(result))
-						}
-					}
+					wg.Add(1)
+					go base.performEvent(request, eventName, ff.Name, val, &f, &wg)
 				}
 				if helpers.IsEmptyValue(f) {
 					continue
 				}
 				tag, ok = ff.Tag.Lookup("load")
+				if !ok {
+					if callerType == CallerGet {
+						tag, ok = ff.Tag.Lookup("get_load")
+					}
+					if !ok {
+						if callerType == CallerPaginate {
+							tag, ok = ff.Tag.Lookup("paginate_load")
+						}
+					}
+				}
 				if ok {
 					tagParts := strings.Split(tag, ",")
 					eventName, targetFieldName := tagParts[0], tagParts[1]
 					val := f.Interface()
-					result, handled := TryEvent(request, eventName, ff.Name, val)
-					if handled && result != nil {
-						targetField := s.FieldByName(targetFieldName)
-						if f.CanSet() {
-							targetField.Set(reflect.ValueOf(result))
-						}
+					targetField := s.FieldByName(targetFieldName)
+					if f.CanSet() {
+						wg.Add(1)
+						go base.performEvent(request, eventName, ff.Name, val, &targetField, &wg)
 					}
 				}
 				switch f.Type().Kind() {
@@ -66,10 +105,12 @@ func (base *BaseDbHandler) handleModelAfterQuery(request models.IRequest, model 
 						break
 					}
 					i := f.Elem()
-					base.handleModelAfterQuery(request, i, true, remainingDepth-1)
+					wg.Add(1)
+					go base.beginHandleModelAfterQuery(request, i, true, remainingDepth-1, &wg)
 					break
 				case reflect.Struct:
-					base.handleModelAfterQuery(request, f, true, remainingDepth-1)
+					wg.Add(1)
+					go base.beginHandleModelAfterQuery(request, f, true, remainingDepth-1, &wg)
 					break
 				}
 			}
@@ -80,6 +121,7 @@ func (base *BaseDbHandler) handleModelAfterQuery(request models.IRequest, model 
 					baseModel.Populate(request)
 				}
 			}
+			wg.Wait()
 			break
 		}
 	}
@@ -90,8 +132,14 @@ func (base *BaseDbHandler) AfterQuery(request models.IRequest, result interface{
 		switch reflect.TypeOf(pr.Items).Kind() {
 		case reflect.Slice:
 			s := reflect.ValueOf(pr.Items)
+			wg := sync.WaitGroup{}
 			for i := 0; i < s.Len(); i++ {
-				base.handleModelAfterQuery(request, s.Index(i), true, 3)
+				wg.Add(1)
+				go func(item reflect.Value) {
+					defer wg.Done()
+					base.handleModelAfterQuery(request, item, true, 3)
+				}(s.Index(i))
+				wg.Wait()
 			}
 		}
 	} else {
@@ -101,6 +149,7 @@ func (base *BaseDbHandler) AfterQuery(request models.IRequest, result interface{
 }
 
 func (base *BaseDbHandler) DoPaginate(request models.IRequest) (result *models.PaginateResult, err error) {
+	request.SetTemp("caller_type", CallerPaginate)
 	err = base.IBaseDbHandler.BeforeQuery(request)
 	if err != nil {
 		return
@@ -117,6 +166,7 @@ func (base *BaseDbHandler) DoPaginate(request models.IRequest) (result *models.P
 }
 
 func (base *BaseDbHandler) DoGet(request models.IRequest) (result models.IBaseModel, err error) {
+	request.SetTemp("caller_type", CallerGet)
 	err = base.IBaseDbHandler.BeforeQuery(request)
 	if err != nil {
 		return
